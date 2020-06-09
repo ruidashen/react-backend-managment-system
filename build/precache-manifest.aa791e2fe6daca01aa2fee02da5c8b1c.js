294b35789e8d020ade51d24eb9e2e88c9e056a8a
ruida shen <shenruida@gmail.com>
2020-06-08 22:33:26 -0700

    production build

---
+++ build/precache-manifest.aa791e2fe6daca01aa2fee02da5c8b1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a7981e4d7956ce08000e1d3b333e27e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75db022f6665085a19f3",
+    "url": "/static/css/2.2a398068.chunk.css"
+  },
+  {
+    "revision": "c3895e5e24e8d97ecf1b",
+    "url": "/static/css/main.ace6f6c1.chunk.css"
+  },
+  {
+    "revision": "75db022f6665085a19f3",
+    "url": "/static/js/2.c68af4c3.chunk.js"
+  },
+  {
+    "revision": "c3895e5e24e8d97ecf1b",
+    "url": "/static/js/main.653452ba.chunk.js"
+  },
+  {
+    "revision": "8a0d3e5eabc32719d60d",
+    "url": "/static/js/runtime-main.9af23e46.js"
+  },
+  {
+    "revision": "124aa9c900e09b9a91a43991e779497c",
+    "url": "/static/media/icon.124aa9c9.png"
+  }
+]);
